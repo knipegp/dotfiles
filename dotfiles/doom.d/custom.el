ad1c0536a3ac0d8971e6f534237f701572389809
Griffin Knipe <knipegp@gmail.com>
2020-09-25 13:00:58 -0400

    Update doom config

--- dotfiles/doom.d/custom.el
+++ dotfiles/doom.d/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-roam org-noter))))
+ '(flycheck-plantuml-executable "plantuml")
+ '(flycheck-textlint-config "~/.textlintrc.yml")
+ '(package-selected-packages '(lsp-metals org-roam org-noter)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- dotfiles/doom.d/init.el
+++ dotfiles/doom.d/init.el
@@ -46,7 +46,7 @@
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
@@ -150,7 +150,7 @@
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       scala             ; java, but good
+       (scala +lsp)             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        (sh +lsp)               ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
