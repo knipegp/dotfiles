f92cb3a06a28b7d9ce10b8988f754b10f1563627
Griffin Knipe <knipegp@gmail.com>
2023-12-03 14:44:25 -0500

    squash

--- gknipe/personal/roles/emacs_editor/files/config.el
+++ gknipe/personal/roles/emacs_editor/files/config.el
@@ -20,6 +20,9 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Fira Code Nerd Font" :size 14))
+(setq doom-variable-pitch-font (font-spec :family "Fira Code Nerd Font" :size 14))
+(setq doom-big-font (font-spec :family "Fira Code Nerd Font" :size 24))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -96,5 +99,5 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 (server-start)
-(add-to-list 'default-frame-alist '(font . "Fira Code" ))
-(set-face-attribute 'default t :font "Fira Code" )
+;; (add-to-list 'default-frame-alist '(font . "Fira Code" ))
+;; (set-face-attribute 'default t :font "Fira Code" )
