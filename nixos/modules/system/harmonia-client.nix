f3ce0a024501210e07bf20fd78c185bb7ce1978a
Griffin Knipe <34732398+knipegp@users.noreply.github.com>
2025-11-18 21:38:46 -0800

    Add nixos cache key (#13)

--- nixos/modules/system/harmonia-client.nix
+++ nixos/modules/system/harmonia-client.nix
@@ -6,6 +6,7 @@
     ];
     trusted-public-keys = [
       "harmonia.haleakala:M2Pu6jHMged1HY4k0MQAJdTMoGBJRXYHZd6h+5NfiQM="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 }
